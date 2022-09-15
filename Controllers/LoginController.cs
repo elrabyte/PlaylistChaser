33f83402874af91d5a0616b92244b4fe8be2057a
Rabyte <tatsusagi+github@gmail.com>
2022-09-15 17:04:44 +0200

    moved login to own controller, renamed some buttons/links

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlaylistChaser.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+        private LoginController db;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult LoginToSpotify(string code)
+        {
+            if (code == null)
+                return Redirect(SpotifyApiHelper.getLoginUri().ToString());
+
+            var spotifyHelper = new SpotifyApiHelper(HttpContext, code);
+            return new JsonResult(new { success = true });
+        }
+    }
+}
