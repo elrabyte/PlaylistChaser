7708bc80fbbfe08eaffa804cc4b30d6e382dbfb8
Shazam <40243053+schazadm@users.noreply.github.com>
2023-08-24 22:29:26 +0200

    added selection mode and combine front ☘🐱‍💻💥

---
+++ PlaylistChaser.Web/Views/Playlist/_CombineYtPlModal.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlaylistChaser.Web.Views.Playlist
+{
+    public class _CombineYtPlModal : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
